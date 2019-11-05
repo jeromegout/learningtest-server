7513bb47b967fc1b23d79b7de4262bd86ae2b777
Jerome Gout <jerome.gout@hcl.com>
2019-11-05 16:25:51 +0100

    - updateUser - login returns AuthData (JWT token and user name) - avatar support - default name get from email address - default avatar get by hashing email address - registerUser checks duplicate user

--- README.md
+++ README.md
@@ -8,14 +8,17 @@ It is defining all API and models.
 * [ ] define model
   * [ ] User
     * [x] registerUser
+      * [x] check duplicate email
     * [x] updateUser
     * [x] login
+      * [x] return logged in user name and JWT token
     * [ ] changeUserPassword
     * [ ] logout
-    
+
   * [ ] Classes
   * [ ] Student
 
 * [x] install NeDB (nedb-promise acutally)
+* [x] install JWT (jsonwebtoken package)
 * [ ] Create a dedicated gmail account to send mail behave Learning Test server
 * [ ] NodeMailer with gmail transport
--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,5 +1,7 @@
 const Datastore = require("nedb-promises")
 const bcrypt = require('bcrypt')
+const User = require('../model/User')
+const jwt = require('jsonwebtoken')
 
 let db = Datastore.create({
   filename: "./db/users.db",
@@ -11,35 +13,54 @@ const resolvers = {
     users: async () => {
       return (await db.find({}).catch(reason => {throw Error(reason)})) || []
     },
-    login: async (_, {nameOrEmail, password}) => {
-      let user
-      if(nameOrEmail.indexOf("@") > 0) {
-        user  = await db.findOne({email:nameOrEmail})
-      } else {
-        user = await db.findOne({name: nameOrEmail})
-      }
+    login: async (_, {input}) => {
+      const user  = await db.findOne({email:input.email})
       if(!user) {
         throw Error('Invalid name or passord, please retry.')
       }
-      
-      const ok = await bcrypt.compare(password, user.password)
+
+      const ok = await bcrypt.compare(input.password, user.password)
       if(!ok) {
         throw Error('Invalid name or passord, please retry.')
       }
 
-      return user
+      const token = jwt.sign(
+        { userId: user._id, email: user.email },
+        '$2b$09$wrGgSVpAbKANR7m6c8OeDqjx5ghozVDpoC8MsKevvgcZg5Fg5',
+        { expiresIn: '1h' }
+      );
+
+      return {
+        userName: user.name,
+        token
+      }
     },
   },
 
   Mutation: {
-    register: async (parent, arg) => {
-      const password = await bcrypt.hash(arg.input.password, 12)
-      const user = {
-        name: arg.input.name,
-        email: arg.input.email,
-        password
+    registerUser: async (parent, {input}) => {
+      const existing  = await db.findOne({email: input.email})
+      if(existing) {
+        throw Error('Account with same email address already exists')
       }
+      const password = await bcrypt.hash(input.password, 12)
+      const user = User.createUser(input.email, password)
       return db.insert(user).catch(reason => {throw Error(reason)});
+    },
+    updateUser: async (_, {userId, input}) => {
+      const existing  = await db.findOne({_id: userId})
+      if(!existing) {
+        throw Error('No such user exist in database')
+      }
+      const user = {
+        ...existing,
+        name: input.name ? input.name : existing.name,
+        email: input.email ? input.email : existing.email,
+        avatar:input.avatar ? input.avatar : existing.avatar
+      }
+      return await db.update({_id: userId}, user, {
+        returnUpdatedDocs: true
+      })
     }
   }
 };
