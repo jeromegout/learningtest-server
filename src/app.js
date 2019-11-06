b0548338f6a64c482e005b0c8dd110fc74eecc38
jerome.gout <jerome.gout@gmail.com>
2019-11-06 01:13:02 +0100

    Authentication  - queries and mutation should found authorization header with the correct token  - login and register are public entry points

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./graphql/schema");
+const resolvers = require("./graphql/resolvers");
+const jwt = require("jsonwebtoken");
 
-const { ApolloServer } = require('apollo-server')
-const typeDefs = require('./graphql/schema')
-const resolvers = require('./graphql/resolvers')
-
-const server = new ApolloServer({ typeDefs, resolvers })
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => {
+    let payload;
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      return { isAuth: false };
+    }
+    const token = authHeader.split(" ")[1];
+    if (!token || token === "") {
+      return { isAuth: false };
+    }
+    try {
+      payload = jwt.verify(token, "somesupersecretkey");
+    } catch (err) {
+      return { isAuth: false };
+    }
+    return { isAuth: true, userId: payload.userId, email: payload.email };
+  },
+});
 
 // The `listen` method launches a web server.
-server.listen().then(({ url }) => { 
+server.listen().then(({ url }) => {
   console.log(`Server ready at ${url}`);
 });
--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,68 +1,80 @@
-const Datastore = require("nedb-promises")
-const bcrypt = require('bcrypt')
-const User = require('../model/User')
-const jwt = require('jsonwebtoken')
+const Datastore = require("nedb-promises");
+const bcrypt = require("bcrypt");
+const User = require("../model/User");
+const jwt = require("jsonwebtoken");
 
 let db = Datastore.create({
   filename: "./db/users.db",
-  autoload: true
+  autoload: true,
 });
 
 const resolvers = {
   Query: {
-    users: async () => {
-      return (await db.find({}).catch(reason => {throw Error(reason)})) || []
+    users: async (_, __, context) => {
+      if (!context.isAuth) {
+        throw Error("Unauthenticated");
+      }
+      return (
+        (await db.find({}).catch(reason => {
+          throw Error(reason);
+        })) || []
+      );
     },
-    login: async (_, {input}) => {
-      const user  = await db.findOne({email:input.email})
-      if(!user) {
-        throw Error('Invalid name or passord, please retry.')
+    login: async (_, { input }) => {
+      const user = await db.findOne({ email: input.email });
+      if (!user) {
+        throw Error("Invalid name or passord, please retry.");
       }
 
-      const ok = await bcrypt.compare(input.password, user.password)
-      if(!ok) {
-        throw Error('Invalid name or passord, please retry.')
+      const ok = await bcrypt.compare(input.password, user.password);
+      if (!ok) {
+        throw Error("Invalid name or passord, please retry.");
       }
 
       const token = jwt.sign(
         { userId: user._id, email: user.email },
-        '$2b$09$wrGgSVpAbKANR7m6c8OeDqjx5ghozVDpoC8MsKevvgcZg5Fg5',
-        { expiresIn: '1h' }
+        "somesupersecretkey",
+        { expiresIn: "1h" }
       );
 
       return {
         userName: user.name,
-        token
-      }
+        token,
+      };
     },
   },
 
   Mutation: {
-    registerUser: async (parent, {input}) => {
-      const existing  = await db.findOne({email: input.email})
-      if(existing) {
-        throw Error('Account with same email address already exists')
+    registerUser: async (parent, { input }) => {
+      const existing = await db.findOne({ email: input.email });
+      if (existing) {
+        throw Error("Account with same email address already exists");
       }
-      const password = await bcrypt.hash(input.password, 12)
-      const user = User.createUser(input.email, password)
-      return db.insert(user).catch(reason => {throw Error(reason)});
+      const password = await bcrypt.hash(input.password, 12);
+      const user = User.createUser(input.email, password);
+      return db.insert(user).catch(reason => {
+        throw Error(reason);
+      });
     },
-    updateUser: async (_, {userId, input}) => {
-      const existing  = await db.findOne({_id: userId})
-      if(!existing) {
-        throw Error('No such user exist in database')
+    updateUser: async (_, { userId, input }, context) => {
+      if (!context.isAuth) {
+        throw Error("Unauthenticated");
+      }
+      const existing = await db.findOne({ _id: userId });
+      if (!existing) {
+        throw Error("No such user exist in database");
       }
       const user = {
         ...existing,
         name: input.name ? input.name : existing.name,
         email: input.email ? input.email : existing.email,
-        avatar:input.avatar ? input.avatar : existing.avatar
-      }
-      return await db.update({_id: userId}, user, {
-        returnUpdatedDocs: true
-      })
-    }
-  }
+        avatar: input.avatar ? input.avatar : existing.avatar,
+      };
+      return await db.update({ _id: userId }, user, {
+        returnUpdatedDocs: true,
+      });
+    },
+  },
 };
 
 module.exports = resolvers;
